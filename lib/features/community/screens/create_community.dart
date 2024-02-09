5bba267c7d922df7a8c4062966a7ddfa3d1529c4
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-02-09 21:08:06 +0100

    create community screen ui

--- lib/features/community/screens/create_community.dart
+++ lib/features/community/screens/create_community.dart
@@ -11,6 +11,45 @@ class CreateCommunity extends ConsumerStatefulWidget {
 class CreateCommunityState extends ConsumerState<CreateCommunity> {
   @override
   Widget build(BuildContext context) {
-    return const Center();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create Community"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text("Community name"),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "r/Community_name",
+                border: InputBorder.none,
+                filled: true,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text(
+                "Create Community",
+                style: TextStyle(fontSize: 17),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
